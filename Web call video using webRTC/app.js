e639ac90232ed0fd63e2d4dbbb52bc27dbb9b6c9
Owais <owaiskhan12969@gmail.com>
2022-10-08 23:36:57 +0500

    New Todo App is added

---
+++ Web call video using webRTC/app.js
@@ -0,0 +1,10 @@
+console.log("Homepage is working");
+
+var mainDivRef = document.getElementById("main_container");
+var streamDiv = document.getElementById("live-streaming");
+var fetchStreaming= document.getElementById("fetch-Streaming");
+var Screenshot = document.getElementById("Screenshot");
+var gallery = document.getElementById("gallery");
+
+
+
